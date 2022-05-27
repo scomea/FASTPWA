38d6ddc8588f53f491239ce40a86904841492884
Stephane Comeau <stephcomeau@msn.com>
2022-05-26 17:47:28 -0700

    better routes

--- src/app-main/routes.ts
+++ src/app-main/routes.ts
@@ -1,8 +1,6 @@
 import { Constructable } from '@microsoft/fast-element';
 import { Container } from '@microsoft/fast-foundation';
 import { RouterConfiguration } from '@microsoft/fast-router';
-import { ArticleView } from "../article-view/article-view";
-import { HomeScreen } from '../home-screen/home-screen';
 import { NotFound } from '../not-found/not-found';
 import { SettingsPanel } from '../settings-panel/settings-panel';
 import { navBarLayout } from '../layouts/nav-bar.layout';
@@ -10,7 +8,6 @@ import { navBarLayout } from '../layouts/nav-bar.layout';
 type RouteSettings = {
   public?: boolean
 };
-
 
 export class MainRouterConfig extends RouterConfiguration<RouteSettings> {
   constructor(@Container private container: Container) {
@@ -22,31 +19,17 @@ export class MainRouterConfig extends RouterConfiguration<RouteSettings> {
     this.defaultLayout = navBarLayout;
     this.routes.map(
       { path: '', redirect: 'home-screen' },
-      { path: 'home-screen', element: HomeScreen, title: 'Home', name: 'home-screen', layout: navBarLayout },
-      { path: 'article', element: ArticleView, title: 'Article', name: 'article', layout: navBarLayout},
-      { path: 'article/{id}', element: ArticleView, title: 'Article', name: 'article', layout: navBarLayout },
+      { path: 'home-screen', element: () => import("../home-screen/home-screen").then(module => new module.HomeScreen()), title: 'Home', name: 'home-screen' },
+      { path: 'article', element: () => import("../article-view/article-view").then(module => new module.ArticleView()), title: 'Article', name: 'article' },
+      { path: 'article/{id}', element: () => import("../article-view/article-view").then(module => new module.ArticleView()), title: 'Article', name: 'article' },
       { path: 'not-found', element: NotFound, title: 'Not Found', name: 'not-found', layout: navBarLayout },
       { path: 'settings-panel', element: SettingsPanel, title: 'Settings', name: 'settings-panel', layout: navBarLayout },
-      { path: 'about-screen', element: () => import("../about-screen/about-screen").then(module => new module.AboutScreen()), title: 'About', name: 'about-screen', layout: navBarLayout },
+      { path: 'about-screen', element: () => import("../about-screen/about-screen").then(module => new module.AboutScreen()), title: 'About', name: 'about-screen' },
     );
 
     this.routes.fallback(
       { redirect: 'not-found' }
     );
-
-    // this.contributors.push({
-    //   navigate(phase) {
-    //     const settings = phase.route.settings;
-
-    //     if (settings && settings.public) {
-    //       return;
-    //     }
-
-    //     phase.cancel(() => {
-    //       Route.name.replace(phase.router, 'login');
-    //     });
-    //   }
-    // });
   }
 
   public construct<T>(Type: Constructable<T>): T {
